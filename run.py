d9402cf307c0657f383617754db6c12cb3e93274
Pratiksha <pratikshatupe51@gmail.com>
2026-07-30 17:32:43 +0530

    testing git configuration

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from app import create_app, db
 app = create_app()
 
 if __name__ == '__main__':
-    # Ensure the database tables are created before starting the server
     with app.app_context():
         db.create_all()
     app.run(debug=True)
